f58c7c787733d1bb38a0dd0459a7a9364c12fe43
José Rodriguez <isc.joserodriguez@gmail.com>
2023-05-10 19:38:15 -0700

    Ejercicio con node

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//CommonJS
+const moment = require('moment');
+
 //Regex
 /* console.log(
   /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w)+$/.test('cadena@blackstone.studio')
@@ -5,7 +8,6 @@
 
 function obtenerEdad(fechaNac) {
   // DD-MM-AAAA
-
   const hoy = new Date();
   const fechaCumple = new Date(fechaNac.split('-').reverse().join('-')); // AAA-MM-DD
   let edad = hoy.getFullYear() - fechaCumple.getFullYear();
@@ -20,4 +22,12 @@ function obtenerEdad(fechaNac) {
   console.log(edad - 1);
 }
 
-obtenerEdad('10-05-2000'); //Expresiones Regulares
+obtenerEdad('10-05-2000');
+
+function obtenerEdadMoment(fechaNac) {
+  const fechaCumple = moment(fechaNac, 'DD-MM-YYYY');
+  const hoy = moment();
+  return hoy.diff(fechaCumple, 'years');
+}
+
+console.log(obtenerEdadMoment('10-06-2000'));
