425ce1f64eca57fb5e7a0a673327f93bb758ff91
José Rodriguez <isc.joserodriguez@gmail.com>
2023-05-10 19:08:15 -0700

    Ejercicio sin node

---
+++ app.js
@@ -0,0 +1,23 @@
+//Regex
+/* console.log(
+  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w)+$/.test('cadena@blackstone.studio')
+); */
+
+function obtenerEdad(fechaNac) {
+  // DD-MM-AAAA
+
+  const hoy = new Date();
+  const fechaCumple = new Date(fechaNac.split('-').reverse().join('-')); // AAA-MM-DD
+  let edad = hoy.getFullYear() - fechaCumple.getFullYear();
+
+  if (
+    hoy.getMonth() >= fechaCumple.getMonth() &&
+    hoy.getDay() >= fechaCumple.getDay()
+  ) {
+    console.log(edad);
+    return;
+  }
+  console.log(edad - 1);
+}
+
+obtenerEdad('10-05-2000'); //Expresiones Regulares
